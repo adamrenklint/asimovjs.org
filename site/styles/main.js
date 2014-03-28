d16194c49ab7a435af99b25171765a52721b77f4
Timothy Achumba <tim@6wunderkinder.com>
2014-03-28 15:46:59 +0000

    Restructured the styles and started the docs page

---
+++ site/styles/main.js
@@ -0,0 +1,15 @@
+// LIBS
+@import 'node_modules/nib'
+
+// GENERAL
+@import "basic"
+
+// PAGES
+@import "home"
+@import "docs"
+
+// CONPONENTS
+@import "header"
+@import "footer"
+@import "menu"
+@import "sidebar"
